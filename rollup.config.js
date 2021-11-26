982084fc4339a2f0d86e4b8f8ffeedd6ebc2e13f
qinjin <qinjin@zhangyue.com>
2021-11-26 19:10:00 +0800

    增加打包方式

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,41 @@
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import html from '@rollup/plugin-html'
+import { terser } from "rollup-plugin-terser";
+import { liveServer } from 'rollup-plugin-live-server'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isProduction = process.env.NODE_ENV === 'production'
+
+let outputFile = './dist/warden.js'
+isDevelopment && (outputFile = './server/warden.js')
 
 export default {
   input: './src/index.js',
   output: {
-    file: './dist/warden.js',
-    format: 'iife'
+    file: outputFile,
+    format: 'es'
   },
   plugins: [
-    resolve({
-      jsnext: true,  // 该属性是指定将Node包转换为ES2015模块
-      // main 和 browser 属性将使插件决定将那些文件应用到bundle中
-      main: true,  // Default: true
-      browser: true // Default: false
-    }),
+    resolve(),
     commonjs(),
     babel({
-      exclude: 'node_modules/**'  // 排除node_module下的所有文件
-    })
+      babelHelpers: 'runtime',
+      presets: [
+        ['@babel/preset-env']
+      ],
+      plugins: [
+        ["@babel/plugin-transform-runtime" ]
+      ],
+      exclude: 'node_modules/**'
+    }),
+    isDevelopment && html(),
+    isDevelopment && liveServer({
+      file: "index.html",
+      mount: [['/server', './server'], ['/src', './src'], ['/node_modules', './node_modules']],
+      open: true
+    }),
+    false && terser()
   ]
 }
